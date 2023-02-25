92bd049212adda4d6db5c6f6299d07e43220165a
Vaibhav Bichave <vaibhavbichave61@gmail.com>
2023-02-25 18:11:39 +0530

    Added ajax and result

--- application/views.py
+++ application/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from . import recommender
-
+from .recommender import recommendation
 from pathlib import Path
 import os
 import pandas as pd
+import pickle
+
+index = []
+pickle.dump(index, open('index.pkl', 'wb'))
+df = pd.read_csv('static/dataset/data_moods.csv')
 
 def index(request):
-        
-    df = pd.read_csv('static/dataset/data_moods.csv')
     dataframe = {}
     for i in range(len(df)):
         data = {}
@@ -15,13 +17,28 @@ def index(request):
         data['artist'] = df['artist'][i]
         data['mood'] = df['mood'][i]
         dataframe[i]=data
-
-    # print(d)
-        # dataframe[i] =  data
-    # print(dataframe)
     df2 = {}
     df2['data'] = dataframe
     return render(request,'index.html', df2)
 
 def result(request):
-    return render(request,'result.html')
+    file = open("index.pkl","rb")
+    index = pickle.load(file)
+    dataframe = {}
+    for i in index:
+        data = {}
+        data['name'] = df['name'][i]
+        data['artist'] = df['artist'][i]
+        data['mood'] = df['mood'][i]
+        dataframe[i]=data
+    df2 = {}
+    df2['data'] = dataframe
+    return render(request,'result.html',df2)
+
+def test(request):
+    if request.method == "GET":
+        song  = list(request.GET.keys())[0]
+        index = recommendation(song,'cosine')
+        pickle.dump(index, open('index.pkl', 'wb'))
+        return result(request)
+    return result(request)
