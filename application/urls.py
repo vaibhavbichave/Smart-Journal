774790e75a15919fdd169ab654cacbcba21d376a
disha4091 <disha4091@gmail.com>
2023-03-10 15:04:43 +0530

    prediction added

--- application/urls.py
+++ application/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('songs/', views.index, name='songs'),
+    # FIXME result page
+    path('songs/', views.songs, name='songs'),
     path('result/', views.result, name='result'),
     path('test/', views.test, name='test'),
-    path('',views.homepage,name='home'),
-    path('journal',views.predict,name='journal'),
-    path('signup/',views.signup,name='signup'),
-    path('login/',views.custom_login,name='login'),
-    path('logout/',views.custom_logout,name='logout'),
+    path('', views.homepage, name='home'),
+    path('predictMood/', views.predictMood, name='predictMood'),
+    path('journal/', views.journal, name='journal'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.custom_login, name='login'),
+    path('logout/', views.custom_logout, name='logout'),
     
 ]
